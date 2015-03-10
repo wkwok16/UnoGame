3ef3b1d437421186e74abad763ef1405320f231d
wkwok16 <wkwok16@punahou.edu>
2015-03-09 21:59:47 -1000

    Uno Game Commit 4
    Lots of updates

--- Card.java
+++ Card.java
@@ -2,26 +2,27 @@ public class Card
 {
   private int type;
   private int number;
-  public Card(int inType, int inNumber)
+  public Card(int inType, int inNumber) // Creates new Card element with a 'type' (color or wild) and 'number' (1-9)
   {
     type = inType;
     number = inNumber;
   }
   
-  public String returnType()
+  public String returnType() // Grabs the type of card input and returns it as a string
   {
-    String[] typeCard = {"Red", "Blue", "Green", "Yellow", " Wild", " Wild Draw 2", " Wild Draw 4"};
+    String[] typeCard = {"Red", "Blue", "Green", "Yellow", "Wild", "Wild Draw 2", "Wild Draw 4"};
     if(type >= 4)
     {
-      int random = (int)(Math.random()*7);
+      int random = (int)(Math.random()*7); // Extra reset for wilds, so they appear less often
       if(random >= 4) number = 0;
       return typeCard[random];
     }
     else return typeCard[type];
   }
   
-  public int returnNumber()
+  public String returnNumber() // Returns number of card as a string
   {
-    return number;
+    if(number != 0) return " "+number;
+    else return "";
   }
 }
--- Player.java
+++ Player.java
@@ -1,13 +1,48 @@
+import java.util.*;
 public class Player
 {
-  Hand playerHand = new Hand();
-  public Player()
+  private ArrayList<Card> hand = new ArrayList<Card>(); // Creates new hand arraylist
+  public Player() // Creates new full 7 card hand at start of game
   {
+    for(int i = 0; i < 7; i++)
+    {
+      hand.add(new Card((int)(Math.random()*7), (int)(Math.random()*9+1)));
+    }
   }
+  
+  public void printHand() // Prints full hand of cards
+  {
+    for(int i = 0; i < hand.size(); i++)
+    {
+      System.out.print("["+hand.get(i).returnType()+hand.get(i).returnNumber()+"]");
+    }
+    System.out.println();
+  }
+  
+  public String playCard(String inText) // Plays card; gets card value from user input
+  {
+    for(int i = 0; i < hand.size(); i++)
+    {
+      if(inText.equals(hand.get(i).returnType()+hand.get(i).returnNumber())) // If card exists...
+      {
+        String returnText = "["+hand.get(i).returnType()+hand.get(i).returnNumber()+"] was played";
+        hand.remove(i); // Removes card from hand arraylist
+        return returnText;
+      }
+    }
+    return "Invalid entry: Card not found"; // If card isn't found, returns this line
+  }
+  
+  public void drawCard() // Adds a new card to the hand
+  {
+    hand.add(new Card((int)(Math.random()*7), (int)(Math.random()*9+1)));
+    this.printHand(); // Prints hand again so player sees their new card
+  }
+  
   
   public String returnstuff()
   {
-    playerHand.printHand();
+    printHand();
     return "";
   }
 }
