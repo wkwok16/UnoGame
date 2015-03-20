070417cce03bd59a2960fe4e00a85a43491b2d0f
wkwok16 <wkwok16@punahou.edu>
2015-03-20 12:42:54 -1000

    Uno Game - Commit 16
    Many bug fixes in an attempt to get computer player running. Also fixed
    many other bugs unintentionally causing problems with computer player
    playing a card.
    
    - Computer player is not yet functioning, yet it is getting a step
    closer to.

--- Computer.java
+++ Computer.java
@@ -1,13 +1,27 @@
 import java.util.*;
 public class Computer extends Player
 {
-  private ArrayList<Card> hand = new ArrayList<Card>(); // Creates new hand arraylist
+  private ArrayList<Card> handComputer = new ArrayList<Card>(); // Creates new hand arraylist
   public Computer() 
   {
     for(int i = 0; i < 7; i++)
     {
-      hand.add(new Card((int)(Math.random()*7), (int)(Math.random()*9+1)));
+      handComputer.add(new Card((int)(Math.random()*7), (int)(Math.random()*9+1)));
     }
+  }
+  
+    public String playCard(String inText) // Plays card; gets card value from user input
+  {
+    for(int i = 0; i < handComputer.size(); i++)
+    {
+      if(inText.equals(handComputer.get(i).returnType()+handComputer.get(i).returnNumber())) // If card exists...
+      {
+        String returnText = "["+handComputer.get(i).returnType()+handComputer.get(i).returnNumber()+"] was played";
+        handComputer.remove(i); // Removes card from hand arraylist
+        return returnText;
+      }
+    }
+    return "Invalid entry: Card not found"; // If card isn't found, returns this line
   }
     
   public String returnCard(String pileTopCard, int in)
@@ -15,37 +29,37 @@ public class Computer extends Player
     String returnCard = "";
     if(pileTopCard.equals(""))
     {
-      int randomCardChooser = ((int)(Math.random()*hand.size()));
-      returnCard = hand.get(randomCardChooser).returnType() + hand.get(randomCardChooser).returnNumber();
-      hand.remove(randomCardChooser);
+      int randomCardChooser = (((int)(Math.random()*handComputer.size())));
+      returnCard = handComputer.get(randomCardChooser).returnType() + handComputer.get(randomCardChooser).returnNumber();
+      handComputer.remove(randomCardChooser);
       return returnCard;
     }
     else 
     {
-      for(int i = 0; i < hand.size(); i++) // Goes through and looks for a playable card
+      for(int i = 0; i < handComputer.size(); i++) // Goes through and looks for a playable card
       {
-        if(pileTopCard.substring(0,1).equals(hand.get(i).returnType()))
+        if(pileTopCard.substring(0,1).equals(handComputer.get(i).returnType()))
         {
-          returnCard = hand.get(i).returnType() + hand.get(i).returnNumber();
-          hand.remove(i);
+          returnCard = handComputer.get(i).returnType() + handComputer.get(i).returnNumber();
+          handComputer.remove(i);
           return returnCard;
         }
       }
-      for(int i = 0; i < hand.size(); i++) // If it cant find a matching letter, find a matching card
+      for(int i = 0; i < handComputer.size(); i++) // If it cant find a matching letter, find a matching card
       {
-        if(pileTopCard.substring(1).equals(hand.get(i).returnNumber()))
+        if(pileTopCard.substring(1).equals(handComputer.get(i).returnNumber()))
         {
-          returnCard = hand.get(i).returnType() + hand.get(i).returnNumber();
-          hand.remove(i);
+          returnCard = handComputer.get(i).returnType() + handComputer.get(i).returnNumber();
+          handComputer.remove(i);
           return returnCard;
         }
       }
-      for(int i = 0; i < hand.size(); i++) // If it cant find a matching number either, return a wild
+      for(int i = 0; i < handComputer.size(); i++) // If it cant find a matching number either, return a wild
       {
-        if(hand.get(i).returnType().substring(0,1).equals("W"))
+        if(handComputer.get(i).returnType().substring(0,1).equals("W"))
         {
-          returnCard = hand.get(i).returnType();
-          hand.remove(i);
+          returnCard = handComputer.get(i).returnType();
+          handComputer.remove(i);
           return returnCard;
         }
       }
